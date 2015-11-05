579b5ccdba20828feea5feb2337e44f1335ad2ad
jrarama <joshuararama@gmail.com>
2015-11-05 23:55:43 +0800

    style: Fixed the spacing/tabs of Gruntfile.js
    Converted tabs from 2 spaces to 4 spaces to be consistent with all JS files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,59 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    concat: {
-        options: {
-            separator: '\n',
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: '\n',
+            },
+            game: {
+                src: [
+                    './js/helpers.js',
+                    './js/resources.js',
+                    './js/entities.js',
+                    './js/engine.js',
+                    './js/app.js'
+                ],
+                dest: './build/game.js',
+            }
         },
-        game: {
-            src: [
-                './js/helpers.js',
-                './js/resources.js',
-                './js/entities.js',
-                './js/engine.js',
-                './js/app.js'
-            ],
-            dest: './build/game.js',
-        }
-    },
-    uglify: {
-        options: {
-            mangle: false,  // Use if you want the names of your functions and variables unchanged
-            compress: true
+        uglify: {
+            options: {
+                mangle: false,    // Use if you want the names of your functions and variables unchanged
+                compress: true
+            },
+            game: {
+                files: {
+                    './build/game.min.js': ['./build/game.js'],
+                }
+            }
         },
-        game: {
-            files: {
-                './build/game.min.js': ['./build/game.js'],
+        jshint: {
+            all: ['./js/**/*.js'],
+            options: {
+                undef: true,
+                curly: true,
+                eqnull: true,
+                predef: ['window', 'document', 'Image', 'Resources',
+                    'Player', 'Block', 'Engine', 'Helpers'
+                ]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['Gruntfile.js', './js/**/*.js'],
+                tasks: ['jshint:all', 'concat:game', 'uglify:game'],
+                options: {
+                    livereload: true
+                },
             }
         }
-    },
-    jshint: {
-      all: ['./js/**/*.js'],
-        options: {
-          undef: true,
-          curly: true,
-          eqnull: true,
-          predef: ['window', 'document', 'Image', 'Resources', 'Player', 'Block', 'Engine', 'Helpers']
-        }
-    },
-    watch: {
-      scripts: {
-        files: ['Gruntfile.js', './js/**/*.js'],
-        tasks: ['jshint:all', 'concat:game', 'uglify:game'],
-        options: {
-          livereload: true
-        },
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['watch']);
 
 };
